f14546787f2122eba3fc9504beb2fe6668e52286
Guest User <Guest@epicodus-70.local>
2018-05-31 12:56:24 -0700

    Detailes to HTML,CSS

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("form#form_01").submit(function(event){
     event.preventDefault();
     var nameInput = $("input#name_info").val();
+    var addressInput = $("input#address_info").val()
     var emailInput = $("input#email_info").val();
     var listInput = $("select#list_info").val();
     var how_manyInput = $("select#how_many").val();
@@ -9,13 +10,17 @@ $(document).ready(function() {
     var how_many_2Input = $("select#how_many_2").val();
 
     $(".name_info").text(nameInput);
+    $(".address_info").text(addressInput);
     $(".email_info").text(emailInput);
     $(".list_info").text(listInput);
     $(".how_many").text(how_manyInput);
     $(".list_info_2").text(list_2Input);
     $(".how_many_2").text(how_many_2Input);
+  });
 
 
+$("button").click(function(){
+    $("form#form_01").hide();
     $("#copy").show();
   });
 });
